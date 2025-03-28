bb8fcd73e18ba6f0cdddd4d2d158d9a5df0e8304
KAVIRAJec <kaviraj.ec21@bitsathy.ac.in>
2025-03-28 21:25:45 +0530

    Completion of Task-7[ADD]

---
+++ Task-7/script.js
@@ -0,0 +1,75 @@
+const chatContainer = document.querySelector('.chat-main');
+const chatItem = document.querySelector('.message');
+
+const input = document.querySelector('.input-text');
+const sendBtn = document.querySelector('.send')
+
+function renderMessages() {
+    try {
+        const messages = JSON.parse(sessionStorage.getItem('messages')) || [];
+        chatContainer.innerHTML = '';
+        messages.forEach((message) => {
+            const messageElement = document.createElement('div')
+            messageElement.classList.add('message', message.sender === 'user' ? 'user' : 'auto')
+            messageElement.innerHTML = 
+                `<div class='message-text'>
+                    <p>${message.text}</p>
+                </div>`
+            chatContainer.prepend(messageElement)
+    })
+    } catch (error) {
+        console.error(error)
+    }
+    
+}
+
+function autoReply(){
+    try {
+        const messages = JSON.parse(sessionStorage.getItem('messages')) || [];
+        const randomMessage = ['Hello', 'How are you?', 'Nice to Here you', 'I am fine', 'Thank you', 'Goodbye'];
+
+        const randomIndex = Math.floor(Math.random() * randomMessage.length);
+        messages.push({ text: randomMessage[randomIndex], sender: 'other' })
+        sessionStorage.setItem('messages', JSON.stringify(messages))
+        renderMessages()
+    } catch (error) {
+        console.error(error)
+    }    
+}
+
+function sendMessage(message) {
+    try {
+        const messages = JSON.parse(sessionStorage.getItem('messages')) || [];
+        messages.push({ text: message, sender: 'user' });
+        sessionStorage.setItem('messages', JSON.stringify(messages));
+        renderMessages();
+        return
+    } catch (error) {
+        console.error(error)
+    }
+}
+
+sendBtn.addEventListener('click', () => {
+    const message = input.value.trim()
+    if (message) {
+        sendMessage(message)
+        setTimeout(autoReply, 2000)
+        input.value = ''
+    } else {
+        throw new Error('Please enter a message');
+    }
+})
+
+input.addEventListener('keydown', (e) => {
+    if (e.key === 'Enter'){
+        const message = input.value.trim()
+    if (message) {
+        sendMessage(message)
+        input.value = ''
+    } else {
+        throw new Error('Please enter a message');
+    }
+    }
+})
+
+document.addEventListener('DOMContentLoaded', renderMessages)
